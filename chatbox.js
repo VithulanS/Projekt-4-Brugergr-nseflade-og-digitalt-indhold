5de37ea37d1f300eb8c1702bb7c5f39e9f38397b
J3003 <jakobandreasen1@gmail.com>
2025-04-08 16:46:21 +0200

    HGeeeej

--- chatbox.js
+++ chatbox.js
@@ -3,23 +3,119 @@
     const closeandlogo = document.getElementById('chatbox__closeandlogo');
     const messagesection = document.getElementById('chatbox__messagesection');
 
-    function openup() {
+    function addMessageYou() {
+        let input = document.getElementById('chatbox__writeinput').value;
+
+
+        if (input.length > 0) {
+            const messageDiv = document.createElement('div');
+            const pTag = document.createElement('p');
+            const Span = document.createElement('span')
+            Span.innerHTML = 'DIG'
+
+            messageDiv.classList.add('chatbox__messageyou')
+            pTag.innerHTML = input
+
+            messageDiv.appendChild(pTag);
+            messageDiv.appendChild(Span)
+            messagesection.appendChild(messageDiv);
+            robotAnswer();
+            scrollToBottom();
+        } else {
+
+        }
+
+
+    }
+
+    function addMessageRobot(text) {
+        const messageDivRobot = document.createElement('div');
+        messageDivRobot.classList.add('chatbox__messagerobot')
+        const pTagForRobot = document.createElement('p')
+
+
+        setTimeout(() => {
+            pTagForRobot.innerHTML = '.';
+            scrollToBottom();
+        }, "250");
+
+        
+        setTimeout(() => {
+            pTagForRobot.innerHTML = '..';
+            scrollToBottom();
+        }, "500");
+
+        setTimeout(() => {
+            pTagForRobot.innerHTML = '...';
+            scrollToBottom();
+        }, "750");
+
+
+        setTimeout(() => {
+            pTagForRobot.innerHTML = text + ' ' + '🤖';
+            scrollToBottom();
+        }, "1000");
+
+        messageDivRobot.appendChild(pTagForRobot);
+        messagesection.appendChild(messageDivRobot);
+        scrollToBottom();
+    }
+
+    function robotAnswer() {
+
+        if (document.getElementById('chatbox__writeinput').value.includes('Hej')) {
+            document.getElementById('chatbox__writeinput').value = '';
+
+            setTimeout(() => {
+                addMessageRobot('Hej du')
+            }, "1000");
+            scrollToBottom();
+        }
+      
+        else {
+            document.getElementById('chatbox__writeinput').value = '';
+            setTimeout(() => {
+                addMessageRobot('Hvad?')
+                scrollToBottom();
+            }, "1000");
+
+        }
+    }
+
+    function clearMessages() {
+        messagesection.innerHTML = '';
+    }
+
+    function openUp() {
         chatbox.classList.toggle('chatbox__udvidet');
         closeandlogo.classList.toggle('chatboxdisplay');
+        clearMessages()
+        addMessageRobot('Hejsa. Mit navn er Sander!')
 
         if (chaticon.style.display === 'flex' || window.getComputedStyle(chaticon).display === 'flex') {
             chaticon.style.display = 'none'; // Hide chaticon
-            messageconveration()
         } else {
             chaticon.style.display = 'flex'; // Show chaticon
         }
 
+
+
     }
+
+
+    function scrollToBottom() {
+        messagesection.scrollTop = messagesection.scrollHeight;
+    }
+
+
+
+
+    /*
 
     function messageconveration() {
         messagesection.innerHTML = ''
         setTimeout(() => {
-            messagesection.innerHTML = '<div id="chatbox__messagerobot">' + '<p>' + 'Hej, mit navn er Sander! 🤖' + '</p>' + '</div>' + '<br>';
+            messagesection.innerHTML = '<div class="chatbox__messagerobot">' + '<p>' + 'Hej, mit navn er Sander! 🤖' + '</p>' + '</div>' + '<br>';
         }, "1000");
 
     }
@@ -39,10 +135,10 @@
     function answers() {
         if (document.getElementById('chatbox__writeinput').value === 'Hej') {
             setTimeout(() => {
-                messagesection.innerHTML += '<div id="chatbox__messagerobot">' + '<p>' + 'Hej ven! 🤖' + '</p>' + '</div>' + '<br>';
+                messagesection.innerHTML += '<div class="chatbox__messagerobot">' + '<p>' + 'Hej ven! 🤖' + '</p>' + '</div>' + '<br>';
 
                 setTimeout(() => {
-                    messagesection.innerHTML += '<div id="chatbox__messagerobot">' + '<p>' + 'Hvad kan jeg gøre for dig? 🤖' + '</p>' + '</div>' + '<br>';
+                    messagesection.innerHTML += '<div class="chatbox__messagerobot">' + '<p>' + 'Hvad kan jeg gøre for dig? 🤖' + '</p>' + '</div>' + '<br>';
                     scrollToBottom();
                 }, "1000");
             }, "500");;
@@ -51,10 +147,10 @@
 
             if (document.getElementById('chatbox__writeinput').value === 'Frivillig?') {
                 setTimeout(() => {
-                    messagesection.innerHTML += '<div id="chatbox__messagerobot">' + '<p>' + 'Øjebilk 🤖' + '</p>' + '</div>' + '<br>';
+                    messagesection.innerHTML += '<div class="chatbox__messagerobot">' + '<p>' + 'Øjebilk 🤖' + '</p>' + '</div>' + '<br>';
     
                     setTimeout(() => {
-                        messagesection.innerHTML += '<div id="chatbox__messagerobot">' + '<p>' + '<b>Klik her</b>' + '</p>' + '</div>' + '<br>';
+                        messagesection.innerHTML += '<div class="chatbox__messagerobot">' + '<p>' + '<b>Klik her</b>' + '</p>' + '</div>' + '<br>';
                         scrollToBottom();
                     }, "1000");
                 }, "500");;
@@ -63,12 +159,12 @@
     
     
                 setTimeout(() => {
-                    messagesection.innerHTML += '<div id="chatbox__messagerobot">' + '<p>' + 'Jeg forstår ikke 🤖' + '</p>' + '</div>' + '<br>';
+                    messagesection.innerHTML += '<div class="chatbox__messagerobot">' + '<p>' + 'Jeg forstår ikke 🤖' + '</p>' + '</div>' + '<br>';
                     scrollToBottom();
     
     
                     setTimeout(() => {
-                        messagesection.innerHTML += '<div id="chatbox__messagerobot">' + '<p>' + 'Sorry ven' + '</p>' + '</div>' + '<br>';
+                        messagesection.innerHTML += '<div class="chatbox__messagerobot">' + '<p>' + 'Sorry ven' + '</p>' + '</div>' + '<br>';
                         scrollToBottom();
                     }, "1000");
     
@@ -80,3 +176,5 @@
         }
         scrollToBottom();
     }
+
+    */
